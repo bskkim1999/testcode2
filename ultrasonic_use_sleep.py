11aea5efbb4252306f3533aff74329da49f0a616
bskkim1999 <124337361+bskkim1999@users.noreply.github.com>
2023-03-28 23:22:33 +0900

    Update ultrasonic_use_sleep.py

--- ultrasonic_use_sleep.py
+++ ultrasonic_use_sleep.py
@@ -46,7 +46,8 @@ if __name__ == '__main__':
             print ("Measured Distance = %.1f cm" % dist)
             time.sleep(1)
  
-        # Reset by pressing CTRL + C
+    
+    # Reset by pressing CTRL + C
     except KeyboardInterrupt:
         print("Measurement stopped by User")
         GPIO.cleanup()
