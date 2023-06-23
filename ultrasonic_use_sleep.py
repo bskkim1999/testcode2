ad655be9959b50693feefba93fb25906ed5247c5
bskkim1999 <124337361+bskkim1999@users.noreply.github.com>
2023-06-23 23:02:03 +0900

    Update ultrasonic_use_sleep.py

--- ultrasonic_use_sleep.py
+++ ultrasonic_use_sleep.py
@@ -64,7 +64,7 @@ if __name__ == '__main__':
         while True:
             dist = distance()
             print ("Measured Distance = %.1f cm" % dist)
-            #time.sleep(0.1)
+            time.sleep(0.1)
             
  
     
